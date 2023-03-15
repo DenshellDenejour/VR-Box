9248a51aba0638e63cee11252c0a863094ba4f05
Denshell Denejour <121324587+DenshellDenejour@users.noreply.github.com>
2023-03-14 22:58:21 -0700

    VR-Box.js
    I make a cube with virtual reality!!

---
+++ VR-Box.js
@@ -0,0 +1,12 @@
+<html>
+ <head> 
+ <script src="https://aframe.io/releases/1.0.3/aframe.min.js"></script> 
+ </head> 
+ <body bgcolor="black"> 
+ <a-scene>
+  <a-box position="-1 0.6 -3" rotation="0 65 0" color="green">
+      
+  </a-box> 
+  </a-scene> 
+  </body> 
+  </html>
